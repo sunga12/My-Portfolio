904c4383ec2dfc6f7ebac4027cba15e1fd44c210
Sunga Thawethe <sungabt@gmail.com>
2023-04-11 11:59:37 +0200

    Add Mobile menu html and script

---
+++ script.js
@@ -0,0 +1,19 @@
+const menu = document.querySelector('.hamburger');
+const navList = document.querySelector('.mob-nav-list')
+const close = document.querySelector('.close');
+const lists = document.querySelectorAll('.mob-nav-list li')
+
+menu.addEventListener('click', () => {
+navList.style.marginLeft = 0;
+});
+
+close.addEventListener('click', () => {
+navList.style.marginLeft = '-1000px';
+});
+
+lists.forEach((list) => {
+list.addEventListener('click', () => {
+    navList.style.marginLeft = '-1000px';
+  });
+
+})
