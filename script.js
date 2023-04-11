98aa43790d3e00f7b075a122c1ae37d81ee8cde0
Sunga Thawethe <sungabt@gmail.com>
2023-04-11 12:29:31 +0200

    Add ESLint config, Fix linter errors

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 const menu = document.querySelector('.hamburger');
-const navList = document.querySelector('.mob-nav-list')
+const navList = document.querySelector('.mob-nav-list');
 const close = document.querySelector('.close');
-const lists = document.querySelectorAll('.mob-nav-list li')
+const lists = document.querySelectorAll('.mob-nav-list li');
 
 menu.addEventListener('click', () => {
-navList.style.marginLeft = 0;
+  navList.style.marginLeft = 0;
 });
 
 close.addEventListener('click', () => {
-navList.style.marginLeft = '-1000px';
+  navList.style.marginLeft = '-1000px';
 });
 
 lists.forEach((list) => {
-list.addEventListener('click', () => {
+  list.addEventListener('click', () => {
     navList.style.marginLeft = '-1000px';
   });
-
-})
+});
