d6ff1eea53893db56bf84e4cb4722fed83ab2431
Lethabo Motloung <106984562+LethaboMot@users.noreply.github.com>
2023-04-12 16:14:39 +0200

    Added the Javascript

--- script.js
+++ script.js
@@ -225,3 +225,120 @@ const projects = [
 
 projects.className = 'projects';
 
+projects.forEach((project) => {
+  const projectdiv = document.createElement('div');
+  projectdiv.className = project.className;
+  projectsSection.appendChild(projectdiv);
+
+  const projTitle = document.createElement('div');
+  projTitle.className = 'professional';
+  projTitle.innerHTML = project.name;
+  projectdiv.appendChild(projTitle);
+
+  const projdesc = document.createElement('div');
+  projdesc.className = 'proj-desc';
+  projdesc.innerHTML = project.description;
+  projectdiv.appendChild(projdesc);
+
+  const projLangList = document.createElement('ul');
+  projLangList.className = 'buttons';
+  projectdiv.appendChild(projLangList);
+
+  const [tech1, tech2, tech3] = project.technologies;
+
+  const projLang1 = document.createElement('li');
+  projLang1.className = 'proj-lang';
+  projLang1.innerHTML = tech1;
+  projLangList.appendChild(projLang1);
+
+  const projLang2 = document.createElement('li');
+  projLang2.className = 'proj-lang';
+  projLang2.innerHTML = tech2;
+  projLangList.appendChild(projLang2);
+
+  const projLang3 = document.createElement('li');
+  projLang3.className = 'proj-lang';
+  projLang3.innerHTML = tech3;
+  projLangList.appendChild(projLang3);
+
+  const seeProject = document.createElement('button');
+  seeProject.innerHTML = '<h2>See Project<h2>';
+  seeProject.className = 'project-link-btn';  
+  seeProject.innerHTML = '<h2>See Project<h2>';
+  projectdiv.appendChild(seeProject);
+seeProject.addEventListener('click', () => {
+    const projectPopup = document.createElement('div');
+    projectPopup.className = 'project-popup';
+    body.appendChild(projectPopup);
+
+    const overlay = document.createElement('div');
+    overlay.id = 'overlay';
+    body.appendChild(overlay);
+
+    const popupTitle = document.createElement('div');
+    popupTitle.className = 'popup-title';
+    popupTitle.innerHTML = project.name;
+    projectPopup.appendChild(popupTitle);
+
+    const popupClose = document.createElement('img');
+    popupClose.className = 'popup-close';
+    popupClose.src = './images/close.png';
+    popupTitle.appendChild(popupClose);
+
+    const popupLangs = document.createElement('ul');
+    popupLangs.className = 'popup-buttons';
+    popupTitle.appendChild(popupLangs);
+
+    const [tech1, tech2, tech3] = project.technologies;
+
+    const popupLang1 = document.createElement('li');
+    popupLang1.className = 'popup-lang';
+    popupLang1.innerHTML = tech1;
+    popupLangs.appendChild(popupLang1);
+
+    const popupLang2 = document.createElement('li');
+    popupLang2.className = 'popup-lang';
+    popupLang2.innerHTML = tech2;
+    popupLangs.appendChild(popupLang2);
+
+    const popupLang3 = document.createElement('li');
+    popupLang3.className = 'popup-lang';
+    popupLang3.innerHTML = tech3;
+    popupLangs.appendChild(popupLang3);
+
+    const popupImgDiv = document.createElement('div');
+    popupImgDiv.className = 'popup-image';
+    projectPopup.appendChild(popupImgDiv);
+
+    const popupImg = document.createElement('img');
+    popupImg.src = project.featuredImage;
+    popupImg.style.width = '100%';
+    popupImg.style.height = '99%';
+    popupImgDiv.appendChild(popupImg);
+
+    const popupDesc = document.createElement('div');
+    popupDesc.className = 'popup-desc';
+    popupDesc.innerHTML = project.description;
+    projectPopup.appendChild(popupDesc);
+
+    const popupSeeMore = document.createElement('div');
+    popupSeeMore.className = 'popup-see-more';
+    popupDesc.appendChild(popupSeeMore);
+
+    const seeLive = document.createElement('button');
+    seeLive.innerHTML = '<h2>See Live <img class="popup-links" src="./images/See-Live.png" alt="Live link"><h2>';
+    seeLive.className = 'see-more';
+    popupSeeMore.appendChild(seeLive);
+
+    const seeSource = document.createElement('button');
+    seeSource.className = 'see-more';
+    seeSource.innerHTML = '<h2>See Source <img class="popup-links" src="./images/github-white-icon.png" alt="github link"><h2>';
+    popupSeeMore.appendChild(seeSource);
+
+    popupClose.addEventListener('click', () => {
+      body.removeChild(projectPopup);
+      body.removeChild(overlay);
+    });
+  })
+
+})
